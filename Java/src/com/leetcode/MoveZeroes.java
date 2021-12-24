9e7ae78b7469081c459f2a1d32eb68faaee25cb9
June <zkitun@163.com>
2021-12-24 20:57:33 +0800

    移动0Java版本

--- Java/src/com/leetcode/MoveZeroes.java
+++ Java/src/com/leetcode/MoveZeroes.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
  * @author june
  */
 public class MoveZeroes {
+
     public static int[] moveZeroes(int[] nums) {
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
